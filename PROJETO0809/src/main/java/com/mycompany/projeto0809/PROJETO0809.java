d5c74c6880e2926d92a5843cf7321ed3bd17fbbf
Werner-Ludwig <20251BSIFSA0020@ifba.edu.br>
2026-09-01 16:44:12 -0300

    Primeiros passos

---
+++ PROJETO0809/src/main/java/com/mycompany/projeto0809/PROJETO0809.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto0809;
+
+/**
+ *
+ * @author werner-ludwig
+ */
+public class PROJETO0809 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
